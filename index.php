1a5a88ae9a188bdb126c279ec3f146d2ba0b3ce2
mjamesderocher <mjamesderocher@gmail.com>
2016-06-01 18:43:49 -0400

    Added info for 2016 camp

--- index.php
+++ index.php
@@ -27,17 +27,17 @@
 								</div>
 							</div>
 						</div>
-						<!--<div class="row">
+						<div class="row">
 							<div class="col-md-8 col-md-offset-2">
 								<div class="content-box">
-									<h2>August 3-7, 2015</h2>
-									<h3><a class="call-to-action" href="/application.php">Apply today!</a></h3>
-									<h3>Registration ends July 29.</h3>
-									<p>You must be between the ages of 11 and 20 years old to apply as a camper. Workers must be at least 21.</p>
-									<p>The fee is $55 for each camper. There is no fee for workers themselves to come to the camp, although some may apply if you want a camp T-shirt or are going to bring younger children with you.</p>
+									<h2>August 1-5, 2016</h2>
+									<h3><a class="call-to-action" href="/appy">Apply today!</a></h3>
+									<h3>Registration ends July 27.</h3>
+									<p>You must be between the ages of 10 and 20 years old to apply as a camper. Workers must be at least 21.</p>
+									<p>Early-bird pricing for each camper is $65 until July 15th, and after that the price is $75. There is no fee for workers themselves to come to the camp, although some may apply if you want a camp T-shirt or are going to bring younger children with you.</p>
 								</div>
 							</div>
-						</div>-->
+						</div>
 						<div class="row">
 							<div class="col-md-8 col-md-offset-2">
 								<div class="content-box">
@@ -48,33 +48,7 @@
 									<h2>Mission</h2>
 									<p>Camp Connexion is all about relationships. It is our desire that the whole body of Christ is joined and held together (Ephesians 6:14) through engaging events that promote personal relationship with God and Christian unity and cooperation among churches, families, and youth.</p>
 									<!--<h2>2015 Theme</h2>
-									<p>You can make it!</p>-->
-								</div>
-							</div>
-						</div>
-						<div class="row">
-							<!--<div class="col-md-8 col-md-offset-2">
-								<div class="content-box">
-									<h2>Schedule</h2>
-									<iframe src="https://www.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;mode=WEEK&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=odmrt0qgfv21njtibbmp2ud814%40group.calendar.google.com&amp;color=%23125A12&amp;ctz=America%2FNew_York;&dates=20150802%2F20150808" style=" border-width:0 " width="100%" height="600" frameborder="0" scrolling="no"></iframe>
-									
-									<h3><a class="call-to-action" href="https://www.google.com/calendar/ical/odmrt0qgfv21njtibbmp2ud814%40group.calendar.google.com/public/basic.ics">Add to your calendar.</a></h3>
-								</div>
-							</div>-->
-							<div class="col-md-8 col-md-offset-2">
-								<div class="content-box">
-									<h2>2015 Pictures</h2>
-									<center>
-										<div id="fb-root"></div><script>(function(d, s, id) {  var js, fjs = d.getElementsByTagName(s)[0];  if (d.getElementById(id)) return;  js = d.createElement(s); js.id = id;  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3";  fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'facebook-jssdk'));</script><div class="fb-post" data-href="https://www.facebook.com/media/set/?set=a.713764655422178.1073741831.670233566441954&amp;type=3" data-width="500"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/media/set/?set=a.713764655422178.1073741831.670233566441954&amp;type=3">Posted by <a href="https://www.facebook.com/connexioncamp">Camp Connexion</a> on&nbsp;<a href="https://www.facebook.com/media/set/?set=a.713764655422178.1073741831.670233566441954&amp;type=3">Monday, August 10, 2015</a></blockquote></div></div>
-									</center>
-									<center>
-										<div id="fb-root"></div><script>(function(d, s, id) {  var js, fjs = d.getElementsByTagName(s)[0];  if (d.getElementById(id)) return;  js = d.createElement(s); js.id = id;  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3";  fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'facebook-jssdk'));</script><div class="fb-post" data-href="https://www.facebook.com/media/set/?set=a.713743462090964.1073741830.670233566441954&amp;type=3" data-width="100%"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/media/set/?set=a.713743462090964.1073741830.670233566441954&amp;type=3"><p>Every morning before chapel we held a spirit rally to see whether the Reds or Blues had more team spirit.</p>Posted by <a href="https://www.facebook.com/connexioncamp">Camp Connexion</a> on&nbsp;<a href="https://www.facebook.com/media/set/?set=a.713743462090964.1073741830.670233566441954&amp;type=3">Monday, August 10, 2015</a></blockquote></div></div></center>
-									<center>
-										<div id="fb-root"></div><script>(function(d, s, id) {  var js, fjs = d.getElementsByTagName(s)[0];  if (d.getElementById(id)) return;  js = d.createElement(s); js.id = id;  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3";  fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'facebook-jssdk'));</script><div class="fb-post" data-href="https://www.facebook.com/media/set/?set=a.714658221999488.1073741832.670233566441954&amp;type=3" data-width="500"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/media/set/?set=a.714658221999488.1073741832.670233566441954&amp;type=3">Posted by <a href="https://www.facebook.com/connexioncamp">Camp Connexion</a> on&nbsp;<a href="https://www.facebook.com/media/set/?set=a.714658221999488.1073741832.670233566441954&amp;type=3">Wednesday, August 12, 2015</a></blockquote></div></div>
-									</center>
-									<center>
-										<div id="fb-root"></div><script>(function(d, s, id) {  var js, fjs = d.getElementsByTagName(s)[0];  if (d.getElementById(id)) return;  js = d.createElement(s); js.id = id;  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3";  fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'facebook-jssdk'));</script><div class="fb-post" data-href="https://www.facebook.com/media/set/?set=a.714671461998164.1073741833.670233566441954&amp;type=3" data-width="500"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/media/set/?set=a.714671461998164.1073741833.670233566441954&amp;type=3">Posted by <a href="https://www.facebook.com/connexioncamp">Camp Connexion</a> on&nbsp;<a href="https://www.facebook.com/media/set/?set=a.714671461998164.1073741833.670233566441954&amp;type=3">Wednesday, August 12, 2015</a></blockquote></div></div>
-									</center>
+										<p>You can make it!</p>-->
 								</div>
 							</div>
 						</div>
@@ -93,7 +67,9 @@
 											<img class="img-responsive img-circle center-block avatar" src="img/matt-shaw.jpg">
 											<h3>Rev. Matthew Shaw</h3>
 											<h4>Co-Founder</h4>
-											<p>Limitations are no obstacle for Rev. Shaw.<!--Over 16 years ago when he took over Brooks Pentecostal Church it had just a congregation of just four people. Now through his leadership it has grown to a congregation of 100 with an active youth group and a private Christian school.--> He has great expectations for what God can do with Camp Connexion.</p>
+											<p>
+												Limitations are no obstacle for Rev. Shaw.<!--Over 16 years ago when he took over Brooks Pentecostal Church it had just a congregation of just four people. Now through his leadership it has grown to a congregation of 100 with an active youth group and a private Christian school.--> He has great expectations for what God can do with Camp Connexion.
+											</p>
 										</div>
 										<div class="col-md-4">
 											<img class="img-responsive img-circle center-block avatar" src="img/kim-shaw.jpg">
@@ -135,7 +111,6 @@
 											<h4>Maintenance Director</h4>
 										</div>
 									</div>
-
 								</div>
 							</div>
 						</div>
@@ -167,6 +142,62 @@
 							</div>
 						</div>
 						<div class="row">
+							<div class="row">
+								<!--<div class="col-md-8 col-md-offset-2">
+									<div class="content-box">
+										<h2>Schedule</h2>
+										<iframe src="https://www.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;mode=WEEK&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=odmrt0qgfv21njtibbmp2ud814%40group.calendar.google.com&amp;color=%23125A12&amp;ctz=America%2FNew_York;&dates=20150802%2F20150808" style=" border-width:0 " width="100%" height="600" frameborder="0" scrolling="no"></iframe>
+										
+										<h3><a class="call-to-action" href="https://www.google.com/calendar/ical/odmrt0qgfv21njtibbmp2ud814%40group.calendar.google.com/public/basic.ics">Add to your calendar.</a></h3>
+									</div>
+								</div>-->
+								<div class="col-md-8 col-md-offset-2">
+									<div class="content-box">
+										<h2>2015 Pictures</h2>
+										<center>
+											<div id="fb-root"></div>
+											<script>(function(d, s, id) {  var js, fjs = d.getElementsByTagName(s)[0];  if (d.getElementById(id)) return;  js = d.createElement(s); js.id = id;  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3";  fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'facebook-jssdk'));</script>
+											<div class="fb-post" data-href="https://www.facebook.com/media/set/?set=a.713764655422178.1073741831.670233566441954&amp;type=3" data-width="500">
+												<div class="fb-xfbml-parse-ignore">
+													<blockquote cite="https://www.facebook.com/media/set/?set=a.713764655422178.1073741831.670233566441954&amp;type=3">Posted by <a href="https://www.facebook.com/connexioncamp">Camp Connexion</a> on&nbsp;<a href="https://www.facebook.com/media/set/?set=a.713764655422178.1073741831.670233566441954&amp;type=3">Monday, August 10, 2015</a></blockquote>
+												</div>
+											</div>
+										</center>
+										<center>
+											<div id="fb-root"></div>
+											<script>(function(d, s, id) {  var js, fjs = d.getElementsByTagName(s)[0];  if (d.getElementById(id)) return;  js = d.createElement(s); js.id = id;  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3";  fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'facebook-jssdk'));</script>
+											<div class="fb-post" data-href="https://www.facebook.com/media/set/?set=a.713743462090964.1073741830.670233566441954&amp;type=3" data-width="100%">
+												<div class="fb-xfbml-parse-ignore">
+													<blockquote cite="https://www.facebook.com/media/set/?set=a.713743462090964.1073741830.670233566441954&amp;type=3">
+														<p>Every morning before chapel we held a spirit rally to see whether the Reds or Blues had more team spirit.</p>
+														Posted by <a href="https://www.facebook.com/connexioncamp">Camp Connexion</a> on&nbsp;<a href="https://www.facebook.com/media/set/?set=a.713743462090964.1073741830.670233566441954&amp;type=3">Monday, August 10, 2015</a>
+													</blockquote>
+												</div>
+											</div>
+										</center>
+										<center>
+											<div id="fb-root"></div>
+											<script>(function(d, s, id) {  var js, fjs = d.getElementsByTagName(s)[0];  if (d.getElementById(id)) return;  js = d.createElement(s); js.id = id;  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3";  fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'facebook-jssdk'));</script>
+											<div class="fb-post" data-href="https://www.facebook.com/media/set/?set=a.714658221999488.1073741832.670233566441954&amp;type=3" data-width="500">
+												<div class="fb-xfbml-parse-ignore">
+													<blockquote cite="https://www.facebook.com/media/set/?set=a.714658221999488.1073741832.670233566441954&amp;type=3">Posted by <a href="https://www.facebook.com/connexioncamp">Camp Connexion</a> on&nbsp;<a href="https://www.facebook.com/media/set/?set=a.714658221999488.1073741832.670233566441954&amp;type=3">Wednesday, August 12, 2015</a></blockquote>
+												</div>
+											</div>
+										</center>
+										<center>
+											<div id="fb-root"></div>
+											<script>(function(d, s, id) {  var js, fjs = d.getElementsByTagName(s)[0];  if (d.getElementById(id)) return;  js = d.createElement(s); js.id = id;  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3";  fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'facebook-jssdk'));</script>
+											<div class="fb-post" data-href="https://www.facebook.com/media/set/?set=a.714671461998164.1073741833.670233566441954&amp;type=3" data-width="500">
+												<div class="fb-xfbml-parse-ignore">
+													<blockquote cite="https://www.facebook.com/media/set/?set=a.714671461998164.1073741833.670233566441954&amp;type=3">Posted by <a href="https://www.facebook.com/connexioncamp">Camp Connexion</a> on&nbsp;<a href="https://www.facebook.com/media/set/?set=a.714671461998164.1073741833.670233566441954&amp;type=3">Wednesday, August 12, 2015</a></blockquote>
+												</div>
+											</div>
+										</center>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="row">
 							<div class="col-md-4 col-md-offset-2">
 								<div class="content-box">
 									<h2>Contact us</h2>
@@ -177,14 +208,14 @@
 								<div class="content-box">
 									<h2>Help the cause</h2>
 									<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-<input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="hosted_button_id" value="GFCDU4WHPTRTE">
-<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+										<input type="hidden" name="cmd" value="_s-xclick">
+										<input type="hidden" name="hosted_button_id" value="GFCDU4WHPTRTE">
+										<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
 										<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 									</form>
 								</div>
 							</div>
-						</div>	
+						</div>
 						<div class="row">
 							<div class="col-md-4 col-md-offset-2">
 								<div class="content-box">
@@ -208,13 +239,13 @@
 									</div>
 								</div>
 							</div>
-							<!--<div class="col-md-4">
+							<div class="col-md-4">
 								<div class="content-box">
 									<h2>Directions</h2>
-									<p>Hosted at Faith School of Theology campus.</p>
+									<p>Hosted at Faith Bible College International campus.</p>
 									<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1454747.3693402824!2d-69.38586913579661!3d44.590818455638306!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0000000000000000%3A0xe265dd78266fd039!2sFaith+School+of+Theology!5e0!3m2!1sen!2sus!4v1431713625555" width="100%" height="450" frameborder="0" style="border:0"></iframe>
 								</div>
-							</div>-->
+								</div>
 						</div>
 						<footer>
 							<a href="http://bible.exchange"><img src="svg/be-logo.svg" class="center-block" id="be-logo"></a>
