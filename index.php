87607c2ee1a7063293813dad88458640c85515fd
Matthew Derocher <mjamesderocher@gmail.com>
2017-06-28 16:19:54 -0400

    Changed donate button

--- index.php
+++ index.php
@@ -166,12 +166,7 @@
 							<div class="col-md-4">
 								<div class="content-box">
 									<h2>Help the cause</h2>
-									<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-										<input type="hidden" name="cmd" value="_s-xclick">
-										<input type="hidden" name="hosted_button_id" value="GFCDU4WHPTRTE">
-										<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-										<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-									</form>
+									<a class="call-to-action" href="https://connexion.camp/apply/donate/">Donate</a>
 								</div>
 							</div>
 						</div>
